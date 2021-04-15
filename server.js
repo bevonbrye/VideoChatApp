8c2d958d1dff941745bb011fd36af02372dae28d
bevonbrye <bryebevon@gmail.com>
2021-04-14 18:24:24 -0700

    add server and dependencies

---
+++ server.js
@@ -0,0 +1,21 @@
+// creates server 
+const express = require('express')
+// runs app function 
+const app = express(); 
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+
+app.set('view engine', 'ejs')
+//contains js 
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => { 
+    
+ }); 
+
+app.get('')
+
+
+server.listen(3000)
