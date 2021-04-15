ca2e4865c7919a3c57622c99403be7e809ced1a1
bevonbrye <bryebevon@gmail.com>
2021-04-15 15:19:05 -0700

    add views update server functions

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // create server 
-const express = require('express')
+const { Console } = require('console');
+const express = require('express');
 // runs app function 
 const app = express(); 
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+//create a dynamic url: 
+const {v4: uuidV4 } = require('uuid');
 
 app.set('view engine', 'ejs')
 //contains js 
@@ -12,10 +14,16 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => { 
-    
+    res.redirect(`/${uuidV4()}`)
  }); 
 
-app.get('')
+app.get('/:room', (req, res) =>{ 
+   res.render('room', { roomId: req.params.room} ) 
+})
 
-
+io.on('connection', socket =>{ 
+    socket.on('join-room', (roomId, userId) =>{
+        console.log(roomId,userId)
+    })
+})
 server.listen(3000)
